abd0f1cda33b2ee159c43d88e8dd75bb18651b1c
eternum-dev <alejandro.thon.j@gmail.com>
2025-05-29 12:24:29 -0400

    feat(layout): add Header component with brand styling

---
+++ front/src/layout/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="flex h-15 p-4 bg-dark items-center shadow-strong border-1 border-b-secondary">
+      <h1 className="font-bold text-2xl text-accent font-display ">Em</h1>
+    </div>
+  );
+};
+
