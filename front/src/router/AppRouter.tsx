3faf82cf16bf52d55775315ce8171f2d81c9560d
eternum-dev <alejandro.thon.j@gmail.com>
2025-06-02 12:53:31 -0400

    feat(router): add AppRouter with route definition

---
+++ front/src/router/AppRouter.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router";
+import { UploadFilesPage, DiffViewerPage, MergeResultPage } from "../pages";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/upload" element={<UploadFilesPage />} />
+      <Route path="/compare" element={<DiffViewerPage />} />
+      <Route path="/result" element={<MergeResultPage />} />
+
+      <Route path={"/*"} element={<Navigate to={"upload"} />} />
+    </Routes>
+  );
+};
