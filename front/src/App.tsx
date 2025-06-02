7e54060eb1fbc11820d1b1a284aef3c7f17a2dd6
eternum-dev <alejandro.thon.j@gmail.com>
2025-06-02 12:54:25 -0400

    feat(app): implement AppRouter in App component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import { Header } from "./layout/Header";
+import { AppRouter } from "./router/AppRouter";
 
 function App() {
   return (
     <div className="app h-full-screen bg-secondary">
       <Header />
-    
+      <AppRouter />
     </div>
   );
 }
