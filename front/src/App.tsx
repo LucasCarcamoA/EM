1bb6e6bf6c673f50549aa1b14ccda188143913f2
eternum-dev <alejandro.thon.j@gmail.com>
2025-06-03 12:52:39 -0400

    hardfix

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./layout/Header";
-import { AppRouter } from "./router/AppRouter";
+// import { AppRouter } from "./router/AppRouter";
+import { UploadFilesPage } from "./pages/UploadFilesPage";
 
 function App() {
   return (
     <div className="app h-full-screen bg-secondary">
       <Header />
-      <AppRouter />
+      {/* <AppRouter /> */}
+      <UploadFilesPage />
     </div>
   );
 }
